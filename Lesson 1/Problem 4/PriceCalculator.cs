0d93cde1a1d1083de1b0191aed70941a9ceda3b7
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-30 00:03:56 +0300

    Add files via upload

---
+++ Lesson 1/Problem 4/PriceCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace _4.HotelReservation
+{
+    public static class PriceCalculator
+    {
+        public static decimal CalculatePrice(decimal pricePerDay,   
+                                             int numberOfDays, 
+                                             Season season, 
+                                             Discount discount) 
+        {
+            int multiplier = (int)season;
+            decimal discountMultiplier = (decimal)discount / 100;
+            decimal priceBeforeDiscount = numberOfDays * pricePerDay * multiplier;
+            decimal discountedAmount = priceBeforeDiscount * discountMultiplier;
+            decimal finalPrice = priceBeforeDiscount - discountedAmount;
+
+            return finalPrice;
+        }
+
+    }
+}
---
+++ Lesson 1/Problem 4/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace _4.HotelReservation
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            decimal pricePerDay = decimal.Parse(input[0]);
+            int numberOfDays = int.Parse(input[1]);
+            string season = input[2];
+
+            Season currentSeason = (Season) Enum.Parse(typeof(Season), season);
+
+            Discount currentDiscount = Discount.None;
+
+            if (input.Length == 4)
+            {
+                currentDiscount = (Discount) Enum.Parse(typeof(Discount), input[3]);
+            }
+
+            decimal totalPrice = PriceCalculator.CalculatePrice(pricePerDay,
+                numberOfDays,
+                currentSeason,
+                currentDiscount);
+
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
