6e67e95d244aceb07c85462898637e3806e6287a
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-30 00:03:05 +0300

    Add files via upload

---
+++ Lesson 1/Problem 2/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿namespace _2.PointInRectangle
+{
+    public class Rectangle
+    {
+        public Point TopLeft { get; set; }
+        public Point BottomRight { get; set; }
+
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            TopLeft = topLeft;
+            BottomRight = bottomRight;
+        }
+
+        public bool Contains(Point point)
+        {
+            return point.CoordinateX >= this.TopLeft.CoordinateX
+                   && point.CoordinateX <= this.BottomRight.CoordinateX
+                   && point.CoordinateY >= this.TopLeft.CoordinateY
+                   && point.CoordinateY <= this.BottomRight.CoordinateY;
+        }
+    }
+}
