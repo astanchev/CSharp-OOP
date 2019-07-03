35ef66ff6d3fe58ec2bbf536edb6d866b43b909e
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-04 00:23:34 +0300

    Add files via upload

---
+++ Lesson 2/Problem 4/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    using System.Collections.Generic;
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            this.random = new Random();
+        }
+
+        public string RandomString()
+        {
+            int index = random.Next(0, this.Count);
+
+            string element = this[index];
+            this.RemoveAt(index);
+
+            return element;
+        }
+    }
+}
