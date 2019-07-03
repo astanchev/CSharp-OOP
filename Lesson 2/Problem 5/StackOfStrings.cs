ddc34c7a04fa02f405ae26bc4c12a88619ef7987
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-04 00:24:11 +0300

    Add files via upload

---
+++ Lesson 2/Problem 5/StackOfStrings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => this.Count == 0;
+
+        public Stack<string> AddRange(Stack<string> stack)
+        {
+            foreach (var element in stack)
+            {
+                this.Push(element);
+            }
+
+            return this;
+        }
+    }
+}
