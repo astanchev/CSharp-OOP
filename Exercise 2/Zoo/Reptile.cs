77adf25cddde2d24bb5d3c642e237aaa140555ed
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-05 23:23:19 +0300

    Add files via upload

---
+++ Exercise 2/Zoo/Reptile.cs
@@ -0,0 +1,13 @@
+﻿namespace Zoo
+{
+    public class Reptile : Animal
+    {
+        public Reptile(string name)
+         :base(name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; protected set; }
+
+    }
+}
