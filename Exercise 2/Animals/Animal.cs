167af3cdb905a4e244802fe7493225d88bdb9d84
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-05 23:16:29 +0300

    Add files via upload

---
+++ Exercise 2/Animals/Animal.cs
@@ -0,0 +1,74 @@
+﻿namespace Animals
+{
+    using System;
+    using System.Text;
+
+    public class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name
+        {
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value == String.Empty)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public virtual string Gender
+        {
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value == String.Empty)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.gender = value;
+            }
+        }
+
+        public virtual string ProduceSound()
+        {
+            return string.Empty;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"{this.GetType().Name}");
+            result.AppendLine($"{this.name} {this.age} {this.gender}");
+            result.AppendLine($"{ProduceSound()}");
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exercise 2/Animals/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace Animals
+{
+    using System;
+    using System.Collections.Generic;
+    public class StartUp
+    {
+        private static List<Animal> animals;
+        public static void Main()
+        {
+            GetAnimals();
+            
+            Console.WriteLine(string.Join(Environment.NewLine, animals));
+        }
+
+        private static void GetAnimals()
+        {
+            animals = new List<Animal>();
+           
+            while (true)
+            {
+                var kind = Console.ReadLine();
+                if (kind == "Beast!")
+                {
+                    break;
+                }
+                
+                string[] animalDetails = Console.ReadLine()
+                                        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = animalDetails[0];
+                int age = int.Parse(animalDetails[1]);
+                string gender = animalDetails[2];
+
+                try
+                {
+                    var animal = AnimalFactory.GetAnimal(kind, name, age, gender);
+                    animals.Add(animal);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+        }
+    }
+}
