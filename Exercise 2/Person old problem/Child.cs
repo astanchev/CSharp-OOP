9ddd911b33aabaced641f2a5b68e2bcad349e3f0
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-05 23:21:00 +0300

    Add files via upload

---
+++ Exercise 2/Person old problem/Child.cs
@@ -0,0 +1,27 @@
+﻿namespace Person
+{
+    using System;
+    public class Child : Person
+    {
+        private const int MaxAge = 15;
+        public Child(string name, int age)
+            :base(name, age)
+        {
+            this.Age = age;
+        }
+
+        public override int Age
+        {
+            get => base.Age; 
+            protected set
+            {
+                if (value > MaxAge)
+                {
+                    throw new ArgumentException($"Child's age must be less than {MaxAge}!");
+                }
+
+                base.Age = value;
+            }
+        }
+    }
+}
---
+++ Exercise 2/Person old problem/Person.cs
@@ -0,0 +1,60 @@
+﻿namespace Person
+{
+    using System;
+    using System.Text;
+    public class Person
+    {
+        private const int MinAge = 0;
+        private const int AllowedNameLengthAbove = 3;
+
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public virtual string Name 
+        {
+            get
+            {
+                return this.name;
+            }
+            protected set
+            {
+                if (value.Length < AllowedNameLengthAbove)
+                {
+                    throw new ArgumentException($"Name's length should not be less than {AllowedNameLengthAbove} symbols!");
+                }
+
+                this.name = value;
+            }
+        }
+        public virtual int Age 
+        {
+            get
+            {
+                return this.age;
+            }
+            protected set
+            {
+                if (value < MinAge)
+                {
+                    throw new ArgumentException("Age must be positive!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append($"Name: {this.Name}, Age: {this.Age}");
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
