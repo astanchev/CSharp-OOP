4cbb48590af11fe867fd081fe847561bb0e32867
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-05 23:22:10 +0300

    Add files via upload

---
+++ Exercise 2/Restaurant/Cake.cs
@@ -0,0 +1,14 @@
+﻿namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double CakeGrams = 250.0;
+        private const double CakeCalories = 1000.0;
+        private const decimal CakePrice = 5.0m;
+
+        public Cake(string name) 
+            : base(name, CakePrice, CakeGrams, CakeCalories)
+        {
+        }
+    }
+}
