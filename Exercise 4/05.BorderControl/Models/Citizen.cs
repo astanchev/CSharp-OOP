15c46d81b874b5d31ba824bde6f73c6008e9f846
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-14 00:39:35 +0300

    Add files via upload

---
+++ Exercise 4/05.BorderControl/Models/Citizen.cs
@@ -0,0 +1,17 @@
+﻿namespace FoodShortage.Models
+{
+    using Interfaces;
+
+    public class Citizen : IIdentifiable
+    {
+        public Citizen(string name, int age, string id)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+    }
+}
