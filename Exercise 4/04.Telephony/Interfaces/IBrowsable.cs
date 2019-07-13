068b8df33893022b18ecfd924c02d7e84758f782
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-13 09:57:55 +0300

    Add files via upload

---
+++ Exercise 4/04.Telephony/Interfaces/IBrowsable.cs
@@ -0,0 +1,7 @@
+﻿namespace _04.Telephony.Interfaces
+{
+    public interface IBrowsable
+    {
+        string Browse(string url);
+    }
+}
---
+++ Exercise 4/04.Telephony/Interfaces/ICallable.cs
@@ -0,0 +1,7 @@
+﻿namespace _04.Telephony.Interfaces
+{
+    public interface ICallable
+    {
+        string Call(string phoneNumber);
+    }
+}
