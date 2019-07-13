a0e6e3d44b890b3b8e6538eef850b92d22183c82
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-14 00:35:43 +0300

    Add files via upload

---
+++ Exercise 4/05.BorderControl/Startup.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodShortage
+{
+    using Core;
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+
+            engine.Run();
+        }
+    }
+}
