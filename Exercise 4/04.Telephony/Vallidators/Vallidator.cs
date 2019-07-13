059ba8f84c241039e12e9db41651f95d3fb40e16
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-13 10:00:30 +0300

    Add files via upload

---
+++ Exercise 4/04.Telephony/Vallidators/Vallidator.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace _04.Telephony.Vallidators
+{
+    public static class Vallidator
+    {
+        public static bool ValidatePhoneNumber(string number)
+        {
+            return number.All(char.IsNumber);
+        }
+
+        public static bool ValidateURL(string url)
+        {
+            return !url.Any(char.IsNumber);
+        }
+    }
+}
