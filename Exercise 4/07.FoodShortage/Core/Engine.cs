8bc1e26b4e2bbb96c7a01b54790c372fb3d37c94
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-14 00:47:40 +0300

    Add files via upload

---
+++ Exercise 4/07.FoodShortage/Core/Engine.cs
@@ -0,0 +1,59 @@
+﻿namespace FoodShortage.Core
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using Factories;
+    using Interfaces;
+
+    public class Engine
+    {
+        private List<IBuyer> population;
+        public Engine()
+        {
+            population = new List<IBuyer>();
+        }
+
+        public void Run()
+        {
+            int numberInputs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberInputs; i++)
+            {
+                string[] commandArgs = Console.ReadLine().Split();
+
+                if (commandArgs.Length == 4)
+                {
+                    IBuyer citizen = CitizenFactory.CreateCitizen(commandArgs);
+
+                    population.Add(citizen);
+                }
+                else if (commandArgs.Length == 3)
+                {
+                    IBuyer rebel = RebelFactory.CreateRebel(commandArgs);
+
+                    population.Add(rebel);
+                }
+            }
+
+            
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "End")
+                {
+                    break;
+                }
+
+                IBuyer buyer = population.FirstOrDefault(b => b.Name == input);
+
+                if (buyer != null)
+                {
+                    buyer.BuyFood();
+                }
+            }
+
+            Console.WriteLine(population.Sum(b => b.Food));
+        }
+    }
+}
