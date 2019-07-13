6c38b596e0469ebf272d15b79ee8bb07bb6b4b7c
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-13 10:01:02 +0300

    Add files via upload

---
+++ Exercise 4/04.Telephony/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _04.Telephony
+{
+    using Core;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+          Engine engine = new Engine();
+
+          engine.Run();
+        }
+    }
+}
