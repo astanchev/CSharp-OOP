8071758c0e35950a364b68455c75770a110217c3
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-14 00:50:02 +0300

    Add files via upload

---
+++ Exercise 4/09.CollectionHierarchy/Contracts/IAddRemovable.cs
@@ -0,0 +1,7 @@
+﻿namespace _09.CollectionHierarchy.Contracts
+{
+    public interface IAddRemovable : IAddable
+    {
+        string Remove();
+    }
+}
---
+++ Exercise 4/09.CollectionHierarchy/Contracts/IUsed.cs
@@ -0,0 +1,7 @@
+﻿namespace _09.CollectionHierarchy.Contracts
+{
+    public interface IUsed : IAddRemovable
+    {
+        int Used { get; }
+    }
+}
