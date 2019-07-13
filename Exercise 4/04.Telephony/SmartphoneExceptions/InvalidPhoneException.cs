e00624cf83e9e7801708cb9c5e1c3e46284450d3
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-13 09:59:37 +0300

    Add files via upload

---
+++ Exercise 4/04.Telephony/SmartphoneExceptions/InvalidPhoneException.cs
@@ -0,0 +1,15 @@
+﻿namespace _04.Telephony.SmartphoneExceptions
+{
+    using System;
+
+    public class InvalidPhoneException : Exception
+    {
+        private const string ExcMessage = "Invalid number!";
+
+        public InvalidPhoneException()
+            :base(ExcMessage)
+        {
+            
+        }
+    }
+}
---
+++ Exercise 4/04.Telephony/SmartphoneExceptions/InvalidURLException.cs
@@ -0,0 +1,14 @@
+﻿namespace _04.Telephony.SmartphoneExceptions
+{
+    using System;
+
+    public class InvalidURLException : Exception
+    {
+        private const string ExcMessage = "Invalid URL!";
+
+        public InvalidURLException()
+            : base(ExcMessage)
+        {
+        }
+    }
+}
