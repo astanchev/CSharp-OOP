b2915e00bd0e236215433337b30df85145395087
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-14 00:49:38 +0300

    Add files via upload

---
+++ Exercise 4/09.CollectionHierarchy/Startup.cs
@@ -0,0 +1,14 @@
+﻿namespace _09.CollectionHierarchy
+{
+    using Core;
+
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+           Engine engine = new Engine();
+
+           engine.Run();
+        }
+    }
+}
