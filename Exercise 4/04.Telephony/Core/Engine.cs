b04241e1355984aa4ee8491762832767048e9ef2
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-13 09:57:06 +0300

    Add files via upload

---
+++ Exercise 4/04.Telephony/Core/Engine.cs
@@ -0,0 +1,66 @@
+﻿namespace _04.Telephony.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Models;
+    using SmartphoneExceptions;
+
+    public class Engine
+    {
+        private List<string> phoneNumbers; 
+        private List<string> urls;
+        private Smartphone smartphone;
+        public Engine()
+        {
+            this.phoneNumbers = new List<string>();
+            this.urls = new List<string>();
+            this.smartphone = new Smartphone();
+        }
+
+        public void Run()
+        {
+            phoneNumbers = ReadInput();
+            urls = ReadInput();
+
+            CallPhones();
+
+            BrowseURLs();
+        }
+
+        private void BrowseURLs()
+        {
+            foreach (var url in urls)
+            {
+                try
+                {
+                    Console.WriteLine(smartphone.Browse(url));
+                }
+                catch (InvalidURLException iue)
+                {
+                    Console.WriteLine(iue.Message);
+                }
+            }
+        }
+
+        private void CallPhones()
+        {
+            foreach (var phoneNumber in phoneNumbers)
+            {
+                try
+                {
+                    Console.WriteLine(smartphone.Call(phoneNumber));
+                }
+                catch (InvalidPhoneException ipe)
+                {
+                    Console.WriteLine(ipe.Message);
+                }
+            }
+        }
+
+        private static List<string> ReadInput()
+        {
+            return Console.ReadLine().Split().ToList();
+        }
+    }
+}
