769c718a3ba48ab0408feae895df342427da69ba
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-13 09:58:42 +0300

    Add files via upload

---
+++ Exercise 4/04.Telephony/Models/Smartphone.cs
@@ -0,0 +1,34 @@
+﻿namespace _04.Telephony.Models
+{
+    using System;
+    using Interfaces;
+    using Vallidators;
+    using SmartphoneExceptions;
+
+    public class Smartphone : ICallable, IBrowsable
+    {
+        public string Call(string phoneNumber)
+        {
+            if (Vallidator.ValidatePhoneNumber(phoneNumber))
+            {
+                return $"Calling... {phoneNumber}";
+            }
+            else
+            {
+                throw new InvalidPhoneException();
+            }
+        }
+
+        public string Browse(string url)
+        {
+            if (Vallidator.ValidateURL(url))
+            {
+                return $"Browsing: {url}!";
+            }
+            else
+            {
+                throw new InvalidURLException();
+            }
+        }
+    }
+}
