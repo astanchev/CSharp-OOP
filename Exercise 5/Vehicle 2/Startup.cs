a437eb5148f28b73708206a0629abb86f159253d
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-17 00:04:34 +0300

    Add files via upload

---
+++ Exercise 5/Vehicle 2/Startup.cs
@@ -0,0 +1,14 @@
+﻿namespace Vehicles
+{
+    using Core;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+
+            engine.Run();
+        }
+    }
+}
