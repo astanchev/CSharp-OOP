47116117a88f8540b135391fc7d4a318bb351ad0
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-17 00:09:35 +0300

    Add files via upload

---
+++ Exercise 4/08.MilitaryElite/Core/Engine.cs
@@ -0,0 +1,68 @@
+﻿namespace MilitaryElite.Core
+{
+    using Factories;
+    using Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Engine
+    {
+        internal static List<Soldier> army;
+        public Engine()
+        {
+            army = new List<Soldier>();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "End")
+                {
+                    break;
+                }
+
+                string[] commandArgs = input.Split();
+                string typeSoldier = commandArgs[0];
+                string id = commandArgs[1];
+                string firstName = commandArgs[2];
+                string lastName = commandArgs[3];
+                decimal salary = decimal.Parse(commandArgs[4]);
+                string[] args = commandArgs.Skip(5).ToArray();
+
+                switch (typeSoldier)
+                {
+                    case "Private":
+                        ArmyFactory.AddPrivateToArmy(id, firstName, lastName, salary);
+                        break;
+                    case "LieutenantGeneral":
+                        ArmyFactory.AddLtGeneralToArmy(id, firstName, lastName, salary, args);
+                        break;
+                    case "Engineer":
+                        ArmyFactory.AddEngineerToArmy(args, id, firstName, lastName, salary);
+                        break;
+                    case "Commando":
+                        ArmyFactory.AddCommandoToArmy(args, id, firstName, lastName, salary);
+                        break;
+                    case "Spy":
+                        ArmyFactory.AddSpyToArmy(salary, id, firstName, lastName);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            PrintOutput();
+        }
+
+        private void PrintOutput()
+        {
+            foreach (var soldier in army)
+            {
+                Console.WriteLine(soldier);
+            }
+        }
+    }
+}
