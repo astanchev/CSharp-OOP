a5647d44f0426d7702f38f49c45e451daaeb5a9f
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-17 00:00:36 +0300

    Add files via upload

---
+++ Exercise 5/Vehicle 1/Core/Engine.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Vehicles.Models;
+
+namespace Vehicles.Core
+{
+    public class Engine
+    {
+        public Engine()
+        {
+            
+        }
+
+        public void Run()
+        {
+            string [] carInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string [] truckInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Vehicle car = new Car(double.Parse(carInfo[1]), double.Parse(carInfo[2]));
+            Vehicle truck = new Truck(double.Parse(truckInfo[1]), double.Parse(truckInfo[2]));
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                string [] command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "Drive")
+                {
+                    if (command[1] == "Car")
+                    {
+                        Console.WriteLine(car.Drive(double.Parse(command[2])));
+                    }
+                    else if (command[1] == "Truck")
+                    {
+                        Console.WriteLine(truck.Drive(double.Parse(command[2])));
+                    }
+                }
+                else if (command[0] == "Refuel")
+                {
+                    if (command[1] == "Car")
+                    {
+                        car.Refuel(double.Parse(command[2]));
+                    }
+                    else if (command[1] == "Truck")
+                    {
+                        truck.Refuel(double.Parse(command[2]));
+                    }
+                }
+            }
+
+            Console.WriteLine(car);
+            Console.WriteLine(truck);
+        }
+    }
+}
