6eda48e787964c420ac8daf727415333c0860590
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-19 12:28:34 +0300

    Add files via upload

---
+++ Exercise 5/Wild Farm/Models/Animals/Mammals/Felines/Cat.cs
@@ -0,0 +1,23 @@
+﻿namespace WildFarm.Models.Animals.Mammals.Felines
+{
+    using Foods;
+    using System.Collections.Generic;
+    public class Cat : Feline
+    {
+        private const double GainValue = 0.3;
+        public Cat(string name, double weight, string livingRegion, string breed) 
+            : base(name, weight, livingRegion, breed)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Meow";
+        }
+
+        public override void Eat(Food food)
+        {
+            this.BaseEat(food, new List<string>(){nameof(Vegetable), nameof(Meat)}, GainValue);
+        }
+    }
+}
