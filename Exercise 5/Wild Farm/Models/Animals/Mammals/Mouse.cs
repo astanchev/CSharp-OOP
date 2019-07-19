517dd2def09f3275a3afd9dc38df35e7e8f7b01d
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-19 12:29:14 +0300

    Add files via upload

---
+++ Exercise 5/Wild Farm/Models/Animals/Mammals/Mouse.cs
@@ -0,0 +1,23 @@
+﻿namespace WildFarm.Models.Animals.Mammals
+{
+    using Foods;
+    using System.Collections.Generic;
+    public class Mouse : Mammal
+    {
+        private const double GainValue = 0.1;
+        public Mouse(string name, double weight, string livingRegion) 
+            : base(name, weight, livingRegion)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Squeak";
+        }
+
+        public override void Eat(Food food)
+        {
+            this.BaseEat(food, new List<string>(){nameof(Vegetable), nameof(Fruit)}, GainValue);
+        }
+    }
+}
