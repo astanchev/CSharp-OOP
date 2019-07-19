a21f8f3ae34ad187357617ea0196e6d1bd158d49
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-19 12:27:11 +0300

    Add files via upload

---
+++ Exercise 5/Wild Farm/Models/Animals/Birds/Owl.cs
@@ -0,0 +1,24 @@
+﻿namespace WildFarm.Models.Animals.Birds
+{
+    using System.Collections.Generic;
+    using Foods;
+    public class Owl : Bird
+    {
+        private const double GainValue = 0.25;
+
+        public Owl(string name, double weight, double wingSize) 
+            : base(name, weight, wingSize)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Hoot Hoot";
+        }
+
+        public override void Eat(Food food)
+        {
+            this.BaseEat(food, new List<string>(){nameof(Meat)}, GainValue);
+        }
+    }
+}
