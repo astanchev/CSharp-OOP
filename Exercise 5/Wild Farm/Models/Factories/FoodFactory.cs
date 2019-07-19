779c6818aa175c54e121b9d8695041cc4c5676ea
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-19 12:30:04 +0300

    Add files via upload

---
+++ Exercise 5/Wild Farm/Models/Factories/FoodFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace WildFarm.Models.Factories
+{
+    using Foods;
+
+    public class FoodFactory
+    {
+        public static Food CreateFood(params string[] foodArgs)
+        {
+            string type = foodArgs[0];
+            int quantity = int.Parse(foodArgs[1]);
+
+            switch (type)
+            {
+                case nameof(Vegetable): return new Vegetable(quantity);
+                case nameof(Fruit): return new Fruit(quantity);
+                case nameof(Meat): return new Meat(quantity);
+                case nameof(Seeds): return new Seeds(quantity);
+                default: return null;
+            }
+        }
+    }
+}
