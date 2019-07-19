6233eb45843d5114418b66bcf8d83221901eecfc
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-19 12:24:37 +0300

    Add files via upload

---
+++ Exercise 5/Wild Farm/Core/Engine.cs
@@ -0,0 +1,54 @@
+﻿namespace WildFarm.Core
+{
+    using Models.Animals;
+    using System;
+    using System.Collections.Generic;
+    using Models.Factories;
+    using Models.Foods;
+    public class Engine
+    {
+        private static List<Animal> animals;
+        public Engine()
+        {
+            animals = new List<Animal>();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "End")
+                {
+                    break;
+                }
+
+                string[] animalArgs = input.Split();
+
+                Animal animal = AnimalFactory.CreateAnimal(animalArgs);
+
+                string[] foodArgs = Console.ReadLine().Split();
+
+                Food food = FoodFactory.CreateFood(foodArgs);
+
+                Console.WriteLine(animal.ProduceSound());
+
+                try
+                {
+                    animal.Eat(food);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+
+                animals.Add(animal);
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
