8144070aac39fcee87d90bfbf830bb15af483dbf
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-22 21:41:54 +0300

    Add files via upload

---
+++ Exercise 6/Logging Library/IOManagement/Entities/IOManager.cs
@@ -0,0 +1,44 @@
+﻿namespace LoggingLibrary.IOManagement.Entities
+{
+    using System.IO;
+
+    using Contracts;
+
+    public class IOManager : IIOManager
+    {
+        private string currentPath;
+        private string currentDirectory;
+        private string currentFile;
+
+        public IOManager()
+        {
+            this.currentPath = this.GetCurrentPath();
+        }
+
+        public IOManager(string currentDirectoryPath, string currentFilePath)
+            :this()
+        {
+            this.currentDirectory = currentDirectoryPath;
+            this.currentFile = currentFilePath;
+        }
+
+        public string CurrentDirectoryPath => Path.Combine(this.currentPath + this.currentDirectory);
+
+        public string CurrentFilePath => Path.Combine(this.CurrentDirectoryPath + this.currentFile);
+
+        public void EnsureDirectoryAndFileExists()
+        {
+            if (!Directory.Exists(this.CurrentDirectoryPath))
+            {
+                Directory.CreateDirectory(this.CurrentDirectoryPath);
+            }
+
+            File.WriteAllText(this.CurrentFilePath, "");
+        }
+
+        public string GetCurrentPath()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+    }
+}
