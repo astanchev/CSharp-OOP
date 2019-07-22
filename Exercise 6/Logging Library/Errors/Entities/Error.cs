a2b4874cf4edbe4c025927b7ebc274f1477c88e4
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-22 21:37:26 +0300

    Add files via upload

---
+++ Exercise 6/Logging Library/Errors/Entities/Error.cs
@@ -0,0 +1,24 @@
+﻿namespace LoggingLibrary.Errors.Entities
+{
+    using System;
+    
+    using Contracts;
+    using Common.Enumerations;
+
+    public class Error : IError
+    {
+        public Error(DateTime dateTime, string message, Level level = Level.INFO)
+        {
+            this.DateTime = dateTime;
+            this.Message = message;
+            this.Level = level;
+        }
+
+        public DateTime DateTime { get; }
+
+        public string Message { get; }
+
+        public Level Level { get; }
+
+    }
+}
