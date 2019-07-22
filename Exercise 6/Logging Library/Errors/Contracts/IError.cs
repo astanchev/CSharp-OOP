8023d3e31bb8ce1c8a550041e29eff36c4c90aa1
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-22 21:36:42 +0300

    Add files via upload

---
+++ Exercise 6/Logging Library/Errors/Contracts/IError.cs
@@ -0,0 +1,15 @@
+﻿namespace LoggingLibrary.Errors.Contracts
+{
+    using System;
+
+    using Common.Enumerations;
+
+    public interface IError
+    {
+        DateTime DateTime { get; }
+
+        string Message { get; }
+
+        Level Level { get; }
+    }
+}
