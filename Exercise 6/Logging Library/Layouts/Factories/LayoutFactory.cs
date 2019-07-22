fc0485432678efa1d0bbadbc8f463acabfbb734a
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-22 21:44:20 +0300

    Add files via upload

---
+++ Exercise 6/Logging Library/Layouts/Factories/LayoutFactory.cs
@@ -0,0 +1,30 @@
+﻿namespace LoggingLibrary.Layouts.Factories
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    using Contracts;
+    using Common.Exceptions;
+    
+    public class LayoutFactory
+    {
+        public ILayout GetLayout(string type)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            Type typeToCreate = assembly
+                                  .GetTypes()
+                                  .FirstOrDefault(t => t.Name == type);
+
+            if (typeToCreate == null)
+            {
+                throw new InvalidLayoutTypeException();
+            }
+
+            ILayout layout = (ILayout)Activator.CreateInstance(typeToCreate);
+            
+            return layout;
+        }
+    }
+}
