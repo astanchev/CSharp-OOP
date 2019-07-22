70417d3787d3ca5cb8aed18b152ffe74630beeb7
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-22 21:38:59 +0300

    Add files via upload

---
+++ Exercise 6/Logging Library/Files/Contracts/IFile.cs
@@ -0,0 +1,14 @@
+﻿namespace LoggingLibrary.Files.Contracts
+{
+    using Errors.Contracts;
+    using Layouts.Contracts;
+
+    public interface IFile
+    {
+        string Path { get; }
+
+        ulong Size { get; }
+
+        string Write(ILayout layout, IError error);
+    }
+}
