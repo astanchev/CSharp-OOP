65b8f0af9544e7acb3b58e6a5931929aa4309c83
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-22 21:46:01 +0300

    Add files via upload

---
+++ Exercise 6/Logging Library/Loggers/Contracts/ILogger.cs
@@ -0,0 +1,14 @@
+﻿namespace LoggingLibrary.Loggers.Contracts
+{
+    using System.Collections.Generic;
+    
+    using Errors.Contracts;
+    using Appenders.Contracts;
+
+    public interface ILogger
+    {
+        IReadOnlyCollection<IAppender> Appenders { get; }
+
+        void Log(IError error);
+    }
+}
