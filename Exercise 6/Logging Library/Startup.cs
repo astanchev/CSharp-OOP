c3bc321bccb567c8db1444b8aa271c8db6ec4108
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-22 21:31:04 +0300

    Add files via upload

---
+++ Exercise 6/Logging Library/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace LoggingLibrary
+{
+    using Core;
+    using Loggers.Contracts;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            ILogger logger = InputReader.GetLogger();
+            
+            Engine engine = new Engine(logger);
+            engine.Run();
+        }
+    }
+}
+
+
