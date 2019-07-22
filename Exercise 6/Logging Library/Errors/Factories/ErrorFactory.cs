f4c153ff26002184d016d9c207d73fe73a1c7a70
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-22 21:37:58 +0300

    Add files via upload

---
+++ Exercise 6/Logging Library/Errors/Factories/ErrorFactory.cs
@@ -0,0 +1,34 @@
+﻿namespace LoggingLibrary.Errors.Factories
+{
+    using System;
+    using System.Globalization;
+
+    using Common;
+    using Common.Exceptions;
+    using Common.Enumerations;
+
+    using Entities;
+    using Contracts;
+
+
+    public class ErrorFactory
+    {
+        public IError GetError(string dateString, string levelStr, string message)
+        {
+            Level level = MessageParameters.ParseLevel(levelStr);
+
+            DateTime dateTime;
+
+            try
+            {
+                dateTime = DateTime.ParseExact(dateString, MessageParameters.dateFormat, CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                throw new InvalidDateFormatException();
+            }
+
+            return new Error(dateTime, message, level);
+        }
+    }
+}
