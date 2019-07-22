ae922be00de4136621efa592529135b276223c21
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-22 21:35:53 +0300

    Add files via upload

---
+++ Exercise 6/Logging Library/Core/Engine.cs
@@ -0,0 +1,54 @@
+﻿namespace LoggingLibrary.Core
+{
+    using System;
+    using Errors.Contracts;
+    using Errors.Factories;
+    using Loggers.Contracts;
+
+    public class Engine
+    {
+        private ILogger logger;
+        private ErrorFactory errorFactory;
+
+        public Engine()
+        {
+            errorFactory = new ErrorFactory();
+        }
+        public Engine(ILogger logger)
+            : this()
+        {
+            this.logger = logger;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+
+                string[] errorArgs = input.Split('|');
+
+                string level = errorArgs[0];
+                string date = errorArgs[1];
+                string message = errorArgs[2];
+
+                try
+                {
+                    IError error = this.errorFactory.GetError(date, level, message);
+
+                    this.logger.Log(error);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine(this.logger.ToString());
+        }
+    }
+}
---
+++ Exercise 6/Logging Library/Core/InputReader.cs
@@ -0,0 +1,57 @@
+﻿namespace LoggingLibrary.Core
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Loggers.Entities;
+    using Loggers.Contracts;
+    using Appenders.Contracts;
+    using Appenders.Factories;
+    
+    public static class InputReader
+    {
+        private static ICollection<IAppender> appenders;
+        private static AppenderFactory appenderFactory;
+
+        public static ILogger GetLogger()
+        {
+            appenders = new List<IAppender>();
+            appenderFactory = new AppenderFactory();
+
+            int appendersCount = int.Parse(Console.ReadLine());
+
+            ReadAppendersData(appendersCount);
+            
+            ILogger logger = new Logger(appenders);
+
+            return logger;
+        }
+        private static void ReadAppendersData(int appendersCount)
+        {
+            for (int i = 0; i < appendersCount; i++)
+            {
+                string[] appendersInfo = Console.ReadLine().Split();
+
+                string appenderType = appendersInfo[0];
+                string layoutType = appendersInfo[1];
+                string levelStr = "INFO";
+
+                if (appendersInfo.Length == 3)
+                {
+                    levelStr = appendersInfo[2];
+                }
+
+                try
+                {
+                    IAppender appender = appenderFactory.GetAppender(appenderType, layoutType, levelStr);
+
+                    appenders.Add(appender);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
