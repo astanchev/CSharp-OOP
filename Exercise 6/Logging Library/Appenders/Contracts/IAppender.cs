75fdccaae61d9a83284f3abb6151f1f33569c305
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-22 21:31:38 +0300

    Add files via upload

---
+++ Exercise 6/Logging Library/Appenders/Contracts/IAppender.cs
@@ -0,0 +1,15 @@
+﻿namespace LoggingLibrary.Appenders.Contracts
+{
+    using Errors.Contracts;
+    using Layouts.Contracts;
+    using Common.Enumerations;
+    
+    public interface IAppender
+    {
+        ILayout Layout { get; }
+
+        Level Level { get; }
+
+        void Append(IError error);
+    }
+}
