64f476bdc38c9470cdd1f686cd715f1d44fef305
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-23 22:57:28 +0300

    Add files via upload

---
+++ Lesson 7/Collector/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _04.Collector
+{
+    using System;
+
+    //No namespace in Judge
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
