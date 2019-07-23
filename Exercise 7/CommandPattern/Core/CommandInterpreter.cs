56a7da4cc5ddca43d8c5ab994bd4440f4e38ecd9
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-24 00:20:50 +0300

    Add files via upload

---
+++ Exercise 7/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,32 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Contracts;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] commandArgs = args.Split();
+
+            string command = commandArgs[0];
+
+            var commandToCreate = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(t => 
+                    t.Name.ToLower() == command.ToLower() + "command");
+
+            if (commandToCreate == null)
+            {
+                throw new ArgumentException("There is no such command");
+            }
+
+            ICommand createdCommand = (ICommand)Activator.CreateInstance(commandToCreate);
+
+           return createdCommand.Execute(commandArgs.Skip(1).ToArray());
+        }
+    }
+}
