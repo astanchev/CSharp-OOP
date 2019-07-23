fe10e63e0f23dc0cda89c0ccbcfdeee2c84dcbb4
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-23 22:56:40 +0300

    Add files via upload

---
+++ Lesson 7/MissionPrivateImpossible/Startup.cs
@@ -0,0 +1,15 @@
+﻿namespace _03.MissionPrivateImpossible
+{
+    using System;
+
+    //No namespace in Judge
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.RevealPrivateMethods("Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
