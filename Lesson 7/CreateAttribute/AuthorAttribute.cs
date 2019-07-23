b34db4f0a7275f404b3690381f5f1b4b1c3f643b
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-23 22:58:20 +0300

    Add files via upload

---
+++ Lesson 7/CreateAttribute/AuthorAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace _05.CreateAttribute
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ Lesson 7/CreateAttribute/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _05.CreateAttribute
+{
+    //No namespace in Judge
+
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
