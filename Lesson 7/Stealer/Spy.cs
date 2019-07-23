a6ec0fcd9670175530252a2925c0aea64ac6f082
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-23 22:55:01 +0300

    Add files via upload

---
+++ Lesson 7/Stealer/Spy.cs
@@ -0,0 +1,37 @@
+﻿namespace _01.Stealer
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] searchedFields)
+        {
+            Type classType = Type.GetType(className);
+
+            FieldInfo[] classFields = classType.GetFields(
+                                BindingFlags.Instance |
+                                          BindingFlags.NonPublic |
+                                          BindingFlags.Public |
+                                          BindingFlags.Static)
+                                .Where(f => searchedFields.Contains(f.Name))
+                                .ToArray();
+
+            Object instance = Activator.CreateInstance(classType, new object[] { });
+
+
+            var builder = new StringBuilder();
+
+            builder.AppendLine($"Class under investigation: {className}");
+
+            foreach (var field in classFields)
+            {
+                builder.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+            }
+
+            return builder.ToString().Trim();
+        } 
+    }
+}
---
+++ Lesson 7/Stealer/Startup.cs
@@ -0,0 +1,15 @@
+﻿namespace _01.Stealer
+{
+    using System;
+
+    //No namespace in Judge
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
