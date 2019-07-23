119c1605c3feb05f9361d2a94c2e6a353f7f5445
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-24 00:25:14 +0300

    Add files via upload

---
+++ Exercise 7/ValidationAttributes/Utilities/Validator.cs
@@ -0,0 +1,35 @@
+﻿namespace ValidationAttributes.Utilities
+{
+    using System.Linq;
+    using System.Reflection;
+    using Attributes;
+
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] propertyInfos = obj
+                                            .GetType()
+                                            .GetProperties();
+
+            foreach (PropertyInfo property in propertyInfos)
+            {
+                MyValidationAttribute[] atts = property
+                                .GetCustomAttributes()
+                                .Where(a => a is MyValidationAttribute)
+                                .Cast<MyValidationAttribute>()
+                                .ToArray();
+
+                foreach (MyValidationAttribute attribute in atts)
+                {
+                    if (!attribute.IsValid(property.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
