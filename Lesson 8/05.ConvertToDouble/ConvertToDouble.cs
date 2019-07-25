171aac3108ba85f6d55241125310ed05ec0d9afb
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-25 09:11:07 +0300

    Add files via upload

---
+++ Lesson 8/05.ConvertToDouble/ConvertToDouble.cs
@@ -0,0 +1,45 @@
+﻿namespace _05.ConvertToDouble
+{
+    using System;
+
+    class ConvertToDouble
+    {
+        static void Main(string[] args)
+        {
+            string value = Console.ReadLine();
+
+            try
+            {
+                double result = ConvertStringToDouble(value);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+
+            Console.ReadLine();
+        }
+
+        private static double ConvertStringToDouble(string value)
+        {
+            double result;
+
+            try 
+            {
+                result = Convert.ToDouble(value);
+                Console.WriteLine($"Converted '{value}' to {result}.");
+            }   
+            catch (FormatException fe) 
+            {
+                throw new FormatException($"Unable to convert '{value}' to a Double.", fe);
+            }               
+            catch (OverflowException ofe)
+            {
+                throw new OverflowException($"'{value}' is outside the range of a Double.", ofe);
+            }
+
+            return result;
+        }
+    }
+}
