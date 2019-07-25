f63be76e920528b07bda94b8f5ef3fa5590a0f0e
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-25 09:10:29 +0300

    Add files via upload

---
+++ Lesson 8/04.FixingVol2/FixingVol2.cs
@@ -0,0 +1,34 @@
+﻿namespace _04.FixingVol2
+{
+    using System;
+
+    class FixingVol2
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            byte result;
+
+            num1 = 30;
+            num2 = 60;
+
+            result = Convert.ToByte(num1 * num2);
+
+            Console.WriteLine($"{num1} x {num2} = {result}");
+
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+
+                Console.WriteLine($"{num1} x {num2} = {result}");
+
+            }
+            catch (OverflowException ofe)
+            {
+                Console.WriteLine("Cannot convert this result to byte! The value is too big.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
