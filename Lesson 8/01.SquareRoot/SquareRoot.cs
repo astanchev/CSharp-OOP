dd97cd3b3856887dde487522071e0b0be91f4a9f
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-25 09:08:44 +0300

    Add files via upload

---
+++ Lesson 8/01.SquareRoot/SquareRoot.cs
@@ -0,0 +1,38 @@
+﻿namespace _01.SquareRoot
+{
+    using System;
+
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                int number = int.Parse(input);
+
+                if (number < 0)
+                {
+                    throw new ArgumentException("Invalid number! - Argument Exception");
+                }
+
+                Console.WriteLine(Math.Sqrt(number));
+
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Invalid number! - Format Exception");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+
+        }
+    }
+}
