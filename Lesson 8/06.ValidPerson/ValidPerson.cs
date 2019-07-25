f3982a658bb43938241b1be48675ceb330e7fcfc
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-25 09:11:44 +0300

    Add files via upload

---
+++ Lesson 8/06.ValidPerson/ValidPerson.cs
@@ -0,0 +1,32 @@
+﻿namespace _06.ValidPerson
+{
+    using System;
+
+    class ValidPerson
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Person pesho = new Person("Pesho","Peshev", 24);
+
+                Person noName = new Person(String.Empty, "Goshev", 31);
+
+                //Person boLastName = new Person("Ivan", String.Empty, 63);
+
+                //Person negativeAge = new Person("Stoyan","Kolev", -1);
+
+                //Person tooOldForThisProgram = new Person("Iskren","Ivanov", 121);
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine($"Exception thrown: {ane.Message}");
+                ;
+            }
+            catch (ArgumentOutOfRangeException aore)
+            {
+                Console.WriteLine($"Exception thrown: {aore.Message}");
+            }
+        }
+    }
+}
