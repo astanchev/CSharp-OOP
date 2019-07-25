e1f7985bef6aa39d09e76746d5e334b248e61589
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-25 09:09:48 +0300

    Add files via upload

---
+++ Lesson 8/03.Fixing/Fixing.cs
@@ -0,0 +1,32 @@
+﻿namespace _03.Fixing
+{
+    using System;
+
+    class Fixing
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDays = new string[5];
+            weekDays[0] = "Sunday";
+            weekDays[1] = "Monday";
+            weekDays[2] = "Tuesday";
+            weekDays[3] = "Wednesday";
+            weekDays[4] = "Thursday";
+
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(weekDays[i].ToString());
+                }
+            }
+            catch (IndexOutOfRangeException iore)
+            {
+                Console.WriteLine("Correct the values range!");
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
