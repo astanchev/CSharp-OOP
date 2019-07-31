aec827f7b3b9eb59717cd0ca1041d0e0711efdff
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-31 12:13:19 +0300

    Add files via upload

---
+++ Lesson 10/Skeleton/fakeTarget.cs
@@ -0,0 +1,24 @@
+﻿namespace Skeleton
+{
+    using Contracts;
+
+    public class FakeTarget : ITarget
+    {
+        public int Health { get; private set; } = 0;
+
+        public int GiveExperience()
+        {
+            return 10;
+        }
+
+        public bool IsDead()
+        {
+            return this.Health <= 0;
+        }
+
+        public void TakeAttack(int attackPoints)
+        {
+            this.Health -= attackPoints;
+        }
+    }
+}
